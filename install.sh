a487abfd017d4334ffc2058def40ed6432dccf6f
Prasad Shirodkar <pshirodkar.work@gmail.com>
2023-10-21 17:32:45 -0700

    optional homebrew installation

--- install.sh
+++ install.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 
+# On mac, first time install: ./install.sh INSTALL_BREW=1
+# On linux, first time install: ./install.sh
+
+INSTALL_BREW=$(INSTALL_BREW:-0)
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 source $SCRIPT_DIR/bash/.functions
 
 pushd $SCRIPT_DIR > /dev/null
 
-if exists brew; then
+if [[ exists brew ]]; then
   brew update && brew upgrade
   brew bundle check
   if [[ $? -ne 0 ]]; then
     brew bundle install --file=$SCRIPT_DIR/Brewfile
   fi
-else
+elif [[ "$INSTALL_BREW" -eq 1 ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   # https://docs.brew.sh/Homebrew-on-Linux
-  if is_linux; then
+  if [[ is_linux ]]; then
     test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
     test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    if exists yum; then
+    if [[ exists yum ]]; then
       sudo yum groupinstall 'Development Tools'
       sudo yum -y install procps-ng curl file git
     fi
@@ -32,7 +37,11 @@ stow --target=$HOME tmux
 stow --target=$HOME screen
 
 # To install useful key bindings and fuzzy completion:
-$(brew --prefix)/opt/fzf/install
+if [[ exists brew ]]; then
+  $(brew --prefix)/opt/fzf/install
+else
+  [[ -x "/usr/local/opt/fzf/install" ]] && . "/usr/local/opt/fzf/install"
+fi
 
 # https://github.com/golang/tools/tree/master/gopls#installation
 exists go && go install golang.org/x/tools/gopls@latest
