0e48f91058a930ca7528022a155fb9609df9062f
Prasad Shirodkar <phirodkar.work@gmail.com>
2025-04-26 16:31:46 -0700

    added helix config

--- stow_source.sh
+++ stow_source.sh
@@ -7,10 +7,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
 pushd $SCRIPT_DIR > /dev/null
 
+mkdir -p $HOME/.config
+
 stow --target=$HOME bash
 stow --target=$HOME vim
 stow --target=$HOME tmux
 stow --target=$HOME screen
+stow --target=$HOME/.config config
 
 source ~/.bashrc
 
