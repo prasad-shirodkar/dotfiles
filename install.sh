15d880170bc25c3d0483fefe6dbe51ab4450fcdd
Prasad Shirodkar <pshirodkar.work@gmail.com>
2025-10-13 15:08:59 -0700

    stow config from install.sh

--- install.sh
+++ install.sh
@@ -53,6 +53,7 @@ stow --target=$HOME bash
 stow --target=$HOME vim
 stow --target=$HOME tmux
 stow --target=$HOME screen
+stow --target=$XDG_CONFIG_HOME config
 
 # https://github.com/golang/tools/tree/master/gopls#installation
 exists go && go install golang.org/x/tools/gopls@latest
